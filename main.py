8c1c979f449e034dfd96dc9a6513724c31e431e3
kostyawh <kostyawh@gmail.com>
2018-10-17 09:30:00 +0700

    add new function

--- main.py
+++ main.py
@@ -9,6 +9,27 @@ def long_process(id, n):
             yield sum
 
 
+def start_many_generators(arr):
+    count = 1
+    R = {}
+    ids = {}
+
+    for i in arr:
+        name = 'Id' + str(count)
+        id = long_process(name, i)
+        R[name] = None
+        ids[name] = id
+        count += 1
+
+    for j in range(256):
+        for k in R.keys():
+            if R[k] is None:
+                R[k] = next(ids[k])
+
+    print(R)
+
+
+# old code
 Id1 = long_process('Id1', 10)
 Id2 = long_process('Id2', 100)
 Id3 = long_process('Id3', 256)
@@ -20,3 +41,12 @@ for i in range(256):
     if R['Id3'] is None: R['Id3'] = next(Id3)
 
 print(R)
+
+# end of old code
+
+
+list = [10, 100, 256, 78]
+
+print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
+start_many_generators(list)
+print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
